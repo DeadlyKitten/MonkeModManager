81b2c730415b27e161ab7bdc9b36879f1455b38b
DeadlyKitten <9684760+DeadlyKitten@users.noreply.github.com>
2021-02-23 18:50:28 -0800

    more cleanup

--- MonkeModManager/FormSelectPlatform.cs
+++ MonkeModManager/FormSelectPlatform.cs
@@ -1,18 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MonkeModManager
 {
     public partial class FormSelectPlatform : Form
     {
-        FormMain Parent;
+        new readonly FormMain Parent;
         public FormSelectPlatform(FormMain parent)
         {
             InitializeComponent();
