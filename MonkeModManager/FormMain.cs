06c0eb4500197100ceaafdace8bc6981a49bb62a
DeadlyKitten <9684760+DeadlyKitten@users.noreply.github.com>
2021-02-27 22:40:14 -0800

    1.2.0

--- MonkeModManager/FormMain.cs
+++ MonkeModManager/FormMain.cs
@@ -19,7 +19,7 @@ namespace MonkeModManager
     {
 
         private const string BaseEndpoint = "https://api.github.com/repos/";
-        private const Int16 CurrentVersion = 3;
+        private const Int16 CurrentVersion = 4;
         private List<ReleaseInfo> releases;
         Dictionary<string, int> groups = new Dictionary<string, int>();
         private string InstallDirectory = @"";
